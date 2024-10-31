3f90b25f4df85988f563b397edb5f9da44e04c8b
ZaimSh <shaqiriximi@gmail.com>
2024-10-31 15:17:49 +0000

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,17 @@
+// Zaim Shaqiri
+// Date: 31.10.2024
+// Programi me demonstrim te hyrjes dhe daljes;
+
+#include <iostream>
+using namespace std;
+
+int main() 
+{ 
+  string emri,mbiemri;
+
+  cout<<"Shtyp emrin dhe mbiemrin :\n";
+  cin>>emri>>mbiemri;
+
+  cout<<"\nPershendetje "<<emri<<" !"<<endl;
+
+}
